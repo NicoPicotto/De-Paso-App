37899bc694eeb979075a4556c433ea4769bcfcc1
NicoPicotto <nicopicotto@hotmail.com>
2022-05-28 13:58:09 -0300

    Image picker

--- src/Screens/NewMoment/index.js
+++ src/Screens/NewMoment/index.js
@@ -1,21 +1,67 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	Image,
+	TextInput,
+	ScrollView,
+} from 'react-native';
+import React, { useState } from 'react';
 import { styles } from './styles';
-import CustomHeader from '../../Components/CustomHeader';
+import * as ImagePicker from 'expo-image-picker';
 
 const NewMoment = ({ navigation }) => {
+	const [image, setImage] = useState(null);
+
+	//Función para seleccionar imagen de la galería
+	const pickImage = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+		if (!result.cancelled) {
+			setImage(result.uri);
+		}
+	};
+
 	const goToList = () => {
 		navigation.navigate('MomentList');
 	};
 
 	return (
 		<View style={styles.container}>
-			<CustomHeader
-				title='Nueva entrada'
-				buttonTitle='Volver'
-				onPressProp={goToList}
-			/>
-			<Text>NewMoment</Text>
+			<TouchableOpacity onPress={pickImage} style={styles.imageButton}>
+				{image ? (
+					<Image source={{ uri: image }} style={styles.image} />
+				) : (
+					<Text>Seleccionar Imagen</Text>
+				)}
+			</TouchableOpacity>
+
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder='Nombre de la entrada...'
+					style={styles.inputName}
+				/>
+				<ScrollView>
+					<TextInput
+						style={styles.inputEntry}
+						multiline={true}
+						numberOfLines={10}
+						placeholder='Texto de la entrada...'
+					/>
+				</ScrollView>
+				<View style={styles.buttonContainer}>
+				<TouchableOpacity style={styles.buttonBack} onPress={goToList}>
+					<Text style={styles.textButtonBack}>VOLVER</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.buttonSave}>
+					<Text style={styles.textButtonSave}>GUARDAR</Text>
+				</TouchableOpacity>
+				</View>
+			</View>
 		</View>
 	);
 };
--- src/Screens/NewMoment/styles.js
+++ src/Screens/NewMoment/styles.js
@@ -4,8 +4,67 @@ import { COLORS } from '../../Constants';
 export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		paddingVertical: 20,
-		paddingHorizontal: 20,
 		backgroundColor: COLORS.terciartyColor,
+	},
+	inputContainer: {
+		padding: 10,
+		flex: 1,
+		backgroundColor: 'white',
+		borderTopLeftRadius: 50,
+		borderTopRightRadius: 50,
+		marginTop: -20,
+		elevation: 5,
+		paddingBottom: 25,
+	},
+	inputName: {
+		fontSize: 22,
+		paddingVertical: 10,
+		marginHorizontal: 20,
+		borderBottomColor: COLORS.terciartyColor,
+		borderBottomWidth: 1,
+		color: COLORS.primaryColor,
+		marginBottom: 5,
+	},
+	inputEntry: {
+		fontSize: 15,
+		marginTop: 5,
+		marginHorizontal: 20,
+		color: COLORS.primaryColor,
+		textAlignVertical: 'top',
+	},
+	imageButton: {
+		height: '50%',
+		overflow: 'hidden',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: COLORS.terciartyColor,
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+	},
+	buttonBack: {
+		paddingVertical: 10,
+		paddingHorizontal: 30,
+		borderRadius: 50,
+		backgroundColor: 'red',
+	},
+	textButtonBack: {
+		color: "white",
+		fontWeight: "bold",
+	},
+	buttonSave: {
+		paddingVertical: 10,
+		paddingHorizontal: 30,
+		borderRadius: 50,
+		backgroundColor: 'green',
+	},
+	textButtonSave: {
+		color: "white",
+		fontWeight: "bold",
 	},
 });
